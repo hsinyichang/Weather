765113ff1c2aa88a43a1783d54285430b965e96e
gina <f1233772002@gmail.com>
2022-08-25 15:31:04 +0800

    氣象資訊完成

---
+++ assets/js/weather.js
@@ -0,0 +1,60 @@
+$('.a1').on("click",function(){
+    $('.dataTables_filter [type=search]').val('新北市').keyup();
+})
+$('.a2').on("click",function(){
+    $('.dataTables_filter [type=search]').val('臺北市').keyup();
+})
+$('.a3').on("click",function(){
+    $('.dataTables_filter [type=search]').val('桃園市').keyup();
+})
+$('.a4').on("click",function(){
+    $('.dataTables_filter [type=search]').val('宜蘭縣').keyup();
+})
+$('.a5').on("click",function(){
+    $('.dataTables_filter [type=search]').val('基隆市').keyup();
+})
+$('.a6').on("click",function(){
+    $('.dataTables_filter [type=search]').val('新竹').keyup();
+})
+$('.b1').on("click",function(){
+    $('.dataTables_filter [type=search]').val('臺中市').keyup();
+})
+$('.b2').on("click",function(){
+    $('.dataTables_filter [type=search]').val('南投縣').keyup();
+})
+$('.b3').on("click",function(){
+    $('.dataTables_filter [type=search]').val('彰化縣').keyup();
+})
+$('.b4').on("click",function(){
+    $('.dataTables_filter [type=search]').val('苗栗縣').keyup();
+})
+$('.b5').on("click",function(){
+    $('.dataTables_filter [type=search]').val('雲林縣').keyup();
+})
+$('.c1').on("click",function(){
+    $('.dataTables_filter [type=search]').val('嘉義').keyup();
+})
+$('.c2').on("click",function(){
+    $('.dataTables_filter [type=search]').val('臺南市').keyup();
+})
+$('.c3').on("click",function(){
+    $('.dataTables_filter [type=search]').val('高雄市').keyup();
+})
+$('.c4').on("click",function(){
+    $('.dataTables_filter [type=search]').val('屏東縣').keyup();
+})
+$('.d1').on("click",function(){
+    $('.dataTables_filter [type=search]').val('花蓮縣').keyup();
+})
+$('.d2').on("click",function(){
+    $('.dataTables_filter [type=search]').val('台東縣').keyup();
+})
+$('.d3').on("click",function(){
+    $('.dataTables_filter [type=search]').val('澎湖縣').keyup();
+})
+$('.d4').on("click",function(){
+    $('.dataTables_filter [type=search]').val('金門縣').keyup();
+})
+$('.d5').on("click",function(){
+    $('.dataTables_filter [type=search]').val('連江縣').keyup();
+})
